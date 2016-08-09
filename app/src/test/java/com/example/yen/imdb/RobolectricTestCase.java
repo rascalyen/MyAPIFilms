e105f9e245a6b5f55ab8584253d30ded91d20126
Yen Huang <rascalyen@gmail.com>
2016-08-08 18:04:00 -0700

    Update: @ignore RobolectricTestCase

--- app/src/test/java/com/example/yen/imdb/RobolectricTestCase.java
+++ app/src/test/java/com/example/yen/imdb/RobolectricTestCase.java
@@ -1,11 +1,13 @@
 package com.example.yen.imdb;
 
 import android.os.Build;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 
+@Ignore
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.M, manifest = "./AndroidManifest.xml")
 public class RobolectricTestCase {}
